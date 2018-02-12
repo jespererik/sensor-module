87f1b2f5cdac9d8a15a669a5f81212a420128b71
Jesper Eriksson <jespererik@users.noreply.github.com>
2018-02-12 10:28:23 +0100

    add library to read from sensor

--- temp-reading.py
+++ temp-reading.py
@@ -1,3 +1,9 @@
-import sys 
+#!/usr/bin/python
+import sys
+import Adafruit_DHT
 
-print ("running from docker container")
+while True:
+
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+
+    print 'Temp: {0:0.1f} C'.format(temperature)
