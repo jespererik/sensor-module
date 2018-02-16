9cb43ed5a8c93dcfda8855cbc6ccb4d0076955a5
Daniel Karlsson <oumu@Daniels-MacBook-Pro.local>
2018-02-16 09:56:55 +0100

    Added REST API for accessing sensor readings

---
+++ RestfulNode.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from DHT11Handler import *
+import threading
+
+
+print "init: Begin"
+data_thread = threading.Thread(target = DHT11DataStream)
+data_thread.start()
+app = Flask(__name__)
+print "init: End"
+
+@app.route('/Temp')
+def getTemp(): return "Temp => " + str(getTemperature())
+
+
+@app.route('/Humdidity')
+def getHumi(): return "Humidity => " + str(getHumidity())
+
+
+@app.route('/All')
+def getData(): return "Humidity & Temperature => " + str(getAll())
+
+if __name__ == '__main__':
+    app.run(debug = True)
