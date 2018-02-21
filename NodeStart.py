0979887a88fb82fcd746a713e7447e4cb7fd4abb
Jesper Eriksson <jespererik@users.noreply.github.com>
2018-02-21 10:06:53 +0100

    removed some debug code

--- NodeStart.py
+++ NodeStart.py
@@ -1,11 +1,11 @@
 from RestfulNode  import *
-#from DHT11Handler import *
+from DHT11Handler import *
 import json
 import threading
 import requests
 import sys
 
-DEBUG = sys.argv[1]
+#DEBUG = sys.argv[1]
 
 nodeInfo = {
    'NODE_ID': '',
@@ -14,30 +14,27 @@ nodeInfo = {
 }
 
 def startThreads():
-   if DEBUG: print 'ENTER => startThreads()'
-   restThread = threading.Thread(target = runRest)
-   #DHT11Thread = threading.Thread(target = DHT11DataStream)
-   
-   restThread.start()
-   #DHT11Thread.start()
-   if DEBUG: print 'EXIT => startThreads()'
+    #if DEBUG: print 'ENTER => startThreads()'
+    restThread = threading.Thread(target = runRest)
+    DHT11Thread = threading.Thread(target = DHT11DataStream)
+    
+    restThread.start()
+    DHT11Thread.start()
+    #if DEBUG: print 'EXIT => startThreads()'
 
 def __init():
-   if DEBUG: print 'ENTER => __Init()'
-   url = 'http://127.0.0.1:5000/init'
+    #if DEBUG: print 'ENTER => __Init()'
+    url = 'http://127.0.0.1:5000/init'
 
-   response = requests.post(url, json=nodeInfo)
-   if response.ok:
-      print 'Server Init Complete'
-      respData = json.loads(response.content)
-      print('Aquired NODE_ID: {}').format(respData['NODE_ID'])
+    response = requests.post(url, json=nodeInfo)
+    if response.ok:
+        print 'Server Init Complete'
+        respData = json.loads(response.content)
+        print('Aquired NODE_ID: {}').format(respData['NODE_ID'])
+    else: 
+        print 'Server Init Failed'
 
-   else: 
-      print 'Server Init Failed'
-
-
-   startThreads()
-   if DEBUG: print 'EXIT => __Init()'
+    startThreads()   
+    #if DEBUG: print 'EXIT => __Init()'
    
-
 __init()
