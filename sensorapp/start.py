67a430eb9798bdafedbda29a509c310848f08b8d
Jesper Eriksson <jespererik@users.noreply.github.com>
2018-03-02 11:32:29 +0100

    change filepaths

--- sensorapp/start.py
+++ sensorapp/start.py
@@ -4,31 +4,40 @@ import requests
 import sys
 from sensorhandler import *
 from restfulapi  import *
+import time
 
-nodeInfo = {
-   'nodeID': '',
+#Add errorhandling to all functions which have a request to the server
+#Where they could get a invalid packet structure response
+
+node_config = {
+   'NODE_NAME': '',
+   'LOCATION': ''
 }
 
-def tryFileOpen(filepath):
+def Try_File_Open(filepath):
     try: 
         open(filepath)
     except IOError:
-        print "Error: file does not appear to exist."
+        print("File does not appear to exist {}".format(filepath))
         sys.exit(1)
 
-def readNodeID():
-    tryFileOpen("/sensor-module/shared/sensor.conf")
-    fopen = open("/sensor-module/shared/sensor.conf", "r")
-    nodeInfo['nodeID'] = fopen.readline()
-    print nodeInfo['nodeID']
-    fopen.close()
+def Read_Node_Config():
+    Try_File_Open("/sensor-module/shared/sensor.conf")
+    with open("/sensor-module/shared/sensor.conf", "r") as conf_file:
+        for element in conf_file.readlines():
+            key, value = element.strip('\n').split(":")
+            node_config[key] = value
+    conf_file.close()
 
-def writeNodeID(nodeID):
-    tryFileOpen("/sensor-module/shared/sensor.conf")
-    fopen = open("/sensor-module/shared/sensor.conf", "w")
-    fopen.write(nodeID)
-    fopen.close()
-
+def Write_Node_Config(new_key, new_value):
+    Try_File_Open("/sensor-module/shared/sensor.conf")
+    with open("/sensor-module/shared/sensor.conf", "w") as conf_file:
+        for key, value in node_config.iteritems():
+            if new_key == key:
+                conf_file.write(key + ':' + new_value + '\n')
+            else:
+                conf_file.write(key + ':' + value + '\n')
+    conf_file.close()
 
 def startThreads():
     restThread = threading.Thread(target = runRest)
@@ -37,33 +46,24 @@ def startThreads():
     restThread.start()
     DHT11Thread.start()
 
-def errorLog(url, err):
-    tryFileOpen("/sensor-module/shared/log/errorlog.log")
-    logfile = open("/sensor-module/shared/log/errorlog.log", "a")
-    logfile.write("Failed to connect to {0}: {1}\n".format(str(url), str(err)))
-    logfile.close()
 
 def __init():
-    url = 'http://192.168.0.121:5000/init'
+    Read_Node_Config()
+    url = "http://192.168.0.121:5000/init"
     while True:
         try:
-            readNodeID()
-            response = requests.post(url, json=nodeInfo)
+            response = requests.post(url, json = node_config)
             response.raise_for_status()
-            print 'Server Init Complete'
-            responseData = json.loads(response.content)
-            if (responseData['nodeID'] !=  nodeInfo['nodeID']):
-                writeNodeID(responseData['nodeID'])
-                print('Aquired nodeID: {}').format(responseData['nodeID'])
+            response_data = json.loads(response.content)
+            print(response_data)
+            if (response_data['NODE_NAME'] !=  node_config['NODE_NAME']):
+                Write_Node_Config('NODE_NAME', response_data['NODE_NAME'])
             else:
                 pass
+            break
         except requests.exceptions.ConnectionError as err:
-            errorLog(url, err)
-            print 'Retry'
-            sleep(10)
+            time.sleep(10)
             continue
-            #sys.exit(1)
-        break
-    startThreads()   
+    Start_Threads()   
     
 __init()
