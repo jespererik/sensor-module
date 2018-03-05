c46482569dbbc5f398ef70b3d30b5e174d8528dc
Daniel Karlsson <oumu@Daniels-MacBook-Pro.local>
2018-03-05 14:42:47 +0100

    Changed variable name node_config -> NODE_CONFIG

--- sensorapp/start.py
+++ sensorapp/start.py
@@ -9,7 +9,7 @@ import time
 #Add errorhandling to all functions which have a request to the server
 #Where they could get a invalid packet structure response
 
-node_config = {
+NODE_CONFIG = {
    'NODE_NAME': '',
    'LOCATION': ''
 }
@@ -26,13 +26,13 @@ def Read_Node_Config():
     with open("/sensor-module/shared/sensor.conf", "r") as conf_file:
         for element in conf_file.readlines():
             key, value = element.strip('\n').split(":")
-            node_config[key] = value
+            NODE_CONFIG[key] = value
     conf_file.close()
 
 def Write_Node_Config(new_key, new_value):
     Try_File_Open("/sensor-module/shared/sensor.conf")
     with open("/sensor-module/shared/sensor.conf", "w") as conf_file:
-        for key, value in node_config.iteritems():
+        for key, value in NODE_CONFIG.iteritems():
             if new_key == key:
                 conf_file.write(key + ':' + new_value + '\n')
             else:
@@ -40,8 +40,8 @@ def Write_Node_Config(new_key, new_value):
     conf_file.close()
 
 def Start_Threads():
-    print(node_config["NODE_NAME"])
-    restThread = threading.Thread(target = runRest, args = (node_config["NODE_NAME"],))
+    print(NODE_CONFIG["NODE_NAME"])
+    restThread = threading.Thread(target = runRest, args = (NODE_CONFIG["NODE_NAME"],))
     DHT11Thread = threading.Thread(target = DHT11DataStream)
     
     restThread.start()
@@ -49,17 +49,17 @@ def Start_Threads():
 
 
 def __init():
-    Read_Node_Config()
+    Read_Node_config()
     url = "http://192.168.0.121:5000/init"
     while True:
         try:
-            response = requests.post(url, json = node_config)
+            response = requests.post(url, json = NODE_CONFIG)
             response.raise_for_status()
             response_data = json.loads(response.content)
-            node_config = response_data
+            NODE_CONFIG = response_data
             print(response_data)
-            if (response_data['NODE_NAME'] !=  node_config['NODE_NAME']):
-                Write_Node_Config('NODE_NAME', response_data['NODE_NAME'])
+            if (response_data['NODE_NAME'] !=  NODE_CONFIG['NODE_NAME']):
+                Write_NODE_CONFIG('NODE_NAME', response_data['NODE_NAME'])
             else:
                 pass
             break
